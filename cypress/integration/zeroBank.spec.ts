ac3528a84200af04e14be9962e771567a0503e48
dharani <dharanidurai54@gmail.com>
2022-07-14 15:23:58 +0530

    feedback

--- cypress/integration/zeroBank.spec.ts
+++ cypress/integration/zeroBank.spec.ts
@@ -1,12 +1,28 @@
 
 describe('Assessment', () => {
-    it('zeroBank Feedback', () => {
+    it('zeroBank Login', () => {
         cy.fixture('assessment/zeroBank.json').then(
             (testdata)=> {
                 const data = testdata
             
         cy.visitHomepage();
-        cy.visitFeedbackpage();
+        cy.get('#signin_button').click()
+        cy.get('#user_login').type(data.username)
+        cy.get('#user_password').type(data.password)
+        cy.get('#user_remember_me').check()
+        cy.get('.btn').click()
+        cy.get(':nth-child(3) > .dropdown-toggle').click()
+        cy.get('#logout_link').click()
+            })
+        })
+
+    it('zeroBank Feedback', () => {
+        cy.fixture('assessment/zeroBank.json').then(
+                (testdata)=> {
+                    const data = testdata
+
+        cy.get('#feedback > div > strong').click()
+        // cy.visitFeedbackpage();
         cy.get('#feedback-title').contains('Feedback');
         cy.get('#name').type(data.name);
         cy.get('#email').type(data.email);
@@ -18,6 +34,7 @@ describe('Assessment', () => {
         cy.get('#subject').type(data.subject);
         cy.get('#comment').type(data.comments)
         cy.get('.btn-signin').click()
+        cy.get('.offset3').contains(data.FeedbackSubmit)
          
               })
         })
